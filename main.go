4da78fa12f153c157aeb2668ec36b023f0e8b554
Andrew Allen <andrewallen2012@gmail.com>
2014-04-28 15:57:21 -0500

    refactor

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func main() {
   for i := 1; i <= 100; i++ {
-    if (i % 3 == 0) && (i % 5 == 0) {
+    if i % 15 == 0 {
       fmt.Println("CracklePop")
-    } else if (i % 3 == 0) {
+    } else if i % 3 == 0 {
       fmt.Println("Crackle")
-    } else if (i % 5 == 0) {
+    } else if i % 5 == 0 {
       fmt.Println("Pop")
     } else {
       fmt.Println(i)
